3664209f58d64e11776debabeea1ba7c76e645c3
ikcilrep <frajerzycki@protonmail.com>
2020-02-12 21:35:28 +0100

    Implement whole Queen

--- Chess/Pieces/Queen.cs
+++ Chess/Pieces/Queen.cs
@@ -5,14 +5,22 @@ namespace Chess
 {
     public class Queen : Piece
     {
+        public Queen(bool color) 
+        {
+            _color = color;
+            _weight = 9;
+        }
+
         public override bool IsMovePossible(Move move, Board board)
         {
-            throw new System.NotImplementedException();
+            return Bishop.IsMovePossibleStatic(move, board) || Rook.IsMovePossibleStatic(move, board);
         }
 
         public override HashSet<Move> PossibleMoves(Board board)
         {
-            throw new System.NotImplementedException();
+            var moves = Bishop.PossibleMovesStatic(this, board);
+            moves.UnionWith(Rook.PossibleMovesStatic(this, board));
+            return moves;
         }
     }
 }
