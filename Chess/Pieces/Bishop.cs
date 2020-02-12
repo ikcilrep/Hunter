dcc159b6a1c22355938f42816fa3469bc20558bc
ikcilrep <frajerzycki@protonmail.com>
2020-02-12 20:16:13 +0100

    Implement PossibleMoves for Bishop

--- Chess/Pieces/Bishop.cs
+++ Chess/Pieces/Bishop.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Chess
 {
@@ -25,9 +27,57 @@ namespace Chess
             }
         }
 
+        private static void AddDiagonalsEnds(Position position, HashSet<Position> diagonalsEnds) {
+            var distance1 = Math.Min(
+                Position.MaxRow - position.Row,
+                Position.MaxColumn - position.Column);
+            var distance2 = Math.Min(
+                position.Row - Position.MinRow,
+                position.Column - Position.MinColumn);
+            var distance3 = Math.Min(
+                Position.MaxRow - position.Row,
+                position.Column - Position.MinColumn);
+            var distance4 = Math.Min(
+                position.Row - Position.MinRow,
+                Position.MaxColumn - position.Column);
+
+            diagonalsEnds.Add(new Position(
+                (byte)(position.Row + distance1),
+                (byte)(position.Column + distance1)));
+            diagonalsEnds.Add(new Position(
+                (byte)(position.Row - distance2),
+                (byte)(position.Column - distance2)));
+            diagonalsEnds.Add(new Position(
+                (byte)(position.Row + distance3),
+                (byte)(position.Column - distance3)));
+            diagonalsEnds.Add(new Position(
+                (byte)(position.Row - distance4),
+                (byte)(position.Column + distance4)));
+        }
+
         public override HashSet<Move> PossibleMoves(Board board)
         {
-            throw new System.NotImplementedException();
+            var position = board.FindPiece(this);
+            var result = new HashSet<Move>();
+            var diagonalsEnds = new HashSet<Position>();
+            AddDiagonalsEnds(position, diagonalsEnds);
+            foreach (var diagonalEnd in diagonalsEnds) 
+            {
+                var diagonal = Positions.Diagonal(position, diagonalEnd);
+                var diagonalFirstEmptyPositions = diagonal.TakeWhile(p => !board.IsTherePiece(p))
+                    .Select(p => new Move(this, p, false));
+                result.UnionWith(diagonalFirstEmptyPositions);
+                if (diagonal.Count > diagonalFirstEmptyPositions.Count())
+                {
+                    var capturePosition = diagonal[diagonalFirstEmptyPositions.Count()];
+                    if (board.IsTherePieceOfColor(capturePosition, !Color))
+                    {
+                        result.Add(new Move(this, capturePosition, true));
+                    }
+                }
+            }
+
+            return result;
         }
     }
 }
