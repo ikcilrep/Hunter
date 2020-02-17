d72a415313b5172deaf74e7f1f8a09eaf72462e0
ikcilrep <frajerzycki@protonmail.com>
2020-02-17 15:35:22 +0100

    Add missing attribute to Promotion

--- Chess/Moves/Promotion.cs
+++ Chess/Moves/Promotion.cs
@@ -7,23 +7,26 @@ namespace Chess.Moves
         public Piece Piece => Pawn;
         public Pawn Pawn { get; }
 
+        public Piece PromotedPawn { get; }
+
         public Position To { get; }
 
         public bool IsCapture { get; }
 
-        public Promotion(Pawn pawn, Position to, Board board)
+        public Promotion(Pawn pawn, Piece promotedPawn, Position to, Board board)
         {
             Pawn = pawn;
+            PromotedPawn = promotedPawn;
             To = to;
             IsCapture = board.IsTherePieceOfColor(To, !Piece.Color);
-        } 
-        
-        public Promotion(Pawn pawn, Position to, bool isCapture)
+        }
+
+        public Promotion(Pawn pawn, Piece promotedPawn, Position to, bool isCapture)
         {
             Pawn = pawn;
             To = to;
             IsCapture = isCapture;
-        } 
+        }
 
 
     }
