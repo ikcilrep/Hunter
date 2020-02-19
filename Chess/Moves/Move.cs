be18d955ab557a1eb607055b7166950515e6a0c6
ikcilrep <frajerzycki@protonmail.com>
2020-02-19 18:45:23 +0100

    Override ToString for Move

--- Chess/Moves/Move.cs
+++ Chess/Moves/Move.cs
@@ -1,4 +1,5 @@
 ﻿using Chess.Pieces;
+using System.Linq;
 
 namespace Chess.Moves
 {
@@ -24,6 +25,36 @@ namespace Chess.Moves
             To = to;
             IsCapture = isCapture;
             Board = board;
+        }
+
+        public override string ToString()
+        {
+            var result = Piece.ToString();
+            var piecePosition = Board.FindPiece(Piece);
+            var movesToTheSamePlace = Board.PossibleMoves.Where(m => m.To == To
+                    && m.Piece.Color == Piece.Color
+                    && m.Piece.GetType() == Piece.GetType()).ToList();
+            if (movesToTheSamePlace.Count == 2)
+            {
+                var otherPiecePosition = Board.FindPiece(movesToTheSamePlace[1].Piece);
+                if (movesToTheSamePlace[1].To.Row == To.Row)
+                {
+                    result += piecePosition.ToString()[0];
+                }
+                else if (movesToTheSamePlace[1].To.Column == To.Column)
+                {
+                    result += piecePosition.ToString()[1];
+                }
+                else
+                {
+                    result += piecePosition.ToString()[0];
+                }
+            }
+            if (IsCapture) {
+                result += "x";
+            }
+            result += To;
+            return result;
         }
 
     }
