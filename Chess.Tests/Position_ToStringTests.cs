f7fe96aa7909d444413fcac5a7b115b3b9a4a5b9
ikcilrep <frajerzycki@protonmail.com>
2020-02-19 19:34:09 +0100

    Add unit test for Position constructor

---
+++ Chess.Tests/Position_ToStringTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+using Chess;
+namespace Chess.Tests
+{
+    public class Position_ToStringTests 
+    {
+        [Test]
+        [TestCase("a4")]
+        [TestCase("c7")]
+        [TestCase("d5")]
+        [TestCase("h8")]
+        [TestCase("a1")]
+        public void ToString_CorrectNotation_ReturnSameField(string notation)
+        {
+            Assert.AreEqual(new Position(notation).ToString(), notation);
+        }
+    }
+}
