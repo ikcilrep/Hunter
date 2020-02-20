cc12a563cc424a31887fd6f6589340f4ef7a9a96
ikcilrep <frajerzycki@protonmail.com>
2020-02-20 18:47:16 +0100

    Add unit test for comparison of different position

--- Chess.Tests/PositionTests.cs
+++ Chess.Tests/PositionTests.cs
@@ -77,5 +77,16 @@ namespace Chess.Tests
             var position2 = new Position(notation);
             Assert.IsTrue(position1 == position2);
         }
+
+        [TestCase("a4", "a3")]
+        [TestCase("c7", "d7")]
+        [TestCase("d5", "b1")]
+        [TestCase("h8", "c8")]
+        public void EqualsOperator_DifferentCoordinatesPositions_ReturnsFalse(string notation1, string notation2)
+        {
+            var position1 = new Position(notation1);
+            var position2 = new Position(notation2);
+            Assert.IsFalse(position1 == position2);
+        }
     }
 }
