de7f1baec932c39df23e5d4fe26967204dad65d3
ikcilrep <frajerzycki@protonmail.com>
2020-02-20 18:56:00 +0100

    Split Position tests into multiple files

---
+++ Chess.Tests/PositionTests.Comaparison.cs
@@ -0,0 +1,31 @@
+using NUnit.Framework;
+using System;
+using Chess;
+namespace Chess.Tests
+{
+    public partial class PositionTests
+    {
+        [TestCase("a4")]
+        [TestCase("c7")]
+        [TestCase("d5")]
+        [TestCase("h8")]
+        [TestCase("a1")]
+        public void EqualsOperator_SameCoordinatesPositions_ReturnsTrue(string notation)
+        {
+            var position1 = new Position(notation);
+            var position2 = new Position(notation);
+            Assert.IsTrue(position1 == position2);
+        }
+
+        [TestCase("a4", "a3")]
+        [TestCase("c7", "d7")]
+        [TestCase("d5", "b1")]
+        [TestCase("h8", "c8")]
+        public void EqualsOperator_DifferentCoordinatesPositions_ReturnsFalse(string notation1, string notation2)
+        {
+            var position1 = new Position(notation1);
+            var position2 = new Position(notation2);
+            Assert.IsFalse(position1 == position2);
+        }
+    }
+}
--- Chess.Tests/PositionTests.Notation.cs
+++ Chess.Tests/PositionTests.Notation.cs
@@ -3,7 +3,7 @@ using System;
 using Chess;
 namespace Chess.Tests
 {
-    public class PositionTests
+    public partial class PositionTests
     {
         [TestCase("a4")]
         [TestCase("c7")]
@@ -66,27 +66,6 @@ namespace Chess.Tests
             Assert.Throws<ArgumentException>(() => new Position((byte)row, (byte)column));
         }
 
-        [TestCase("a4")]
-        [TestCase("c7")]
-        [TestCase("d5")]
-        [TestCase("h8")]
-        [TestCase("a1")]
-        public void EqualsOperator_SameCoordinatesPositions_ReturnsTrue(string notation)
-        {
-            var position1 = new Position(notation);
-            var position2 = new Position(notation);
-            Assert.IsTrue(position1 == position2);
-        }
-
-        [TestCase("a4", "a3")]
-        [TestCase("c7", "d7")]
-        [TestCase("d5", "b1")]
-        [TestCase("h8", "c8")]
-        public void EqualsOperator_DifferentCoordinatesPositions_ReturnsFalse(string notation1, string notation2)
-        {
-            var position1 = new Position(notation1);
-            var position2 = new Position(notation2);
-            Assert.IsFalse(position1 == position2);
-        }
+        
     }
 }
