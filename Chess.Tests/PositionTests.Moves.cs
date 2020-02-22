48190b05cee617250751f900f2aa42f4690973c2
ikcilrep <frajerzycki@protonmail.com>
2020-02-22 14:56:20 +0100

    Add unit test for Position.Forward correct situation

---
+++ Chess.Tests/PositionTests.Moves.cs
@@ -0,0 +1,16 @@
+using NUnit.Framework;
+namespace Chess.Tests
+{
+    [TestFixture]
+    public partial class PositionTests
+    {
+        [TestCase("e2", Board.White, 2, 3)]
+        [TestCase("a1", Board.White, 7, 7)]
+        [TestCase("h8", Board.Black, 3, 4)]
+        [TestCase("a2", Board.Black, 1, 0)]
+        public void Forward_CorrectSituation_ReturnsExpectedRow(string notation, bool color, int distance, int expectedRow) {
+            var position = new Position(notation);
+            Assert.AreEqual((byte)expectedRow, position.Forward((byte) distance, color));
+        }  
+    }
+}
