ee0d0ad9aa6c948c1fa3043ad7e9b1db10d17af2
ikcilrep <frajerzycki@protonmail.com>
2020-02-22 16:48:33 +0100

    Add unit test for Position.GoInDirectionOf Incorrect case of usage is tested by constructor test.

--- Chess.Tests/PositionTests.Moves.cs
+++ Chess.Tests/PositionTests.Moves.cs
@@ -12,5 +12,15 @@ namespace Chess.Tests
             var position = new Position(positionString);
             Assert.AreEqual((byte)expectedRow, position.Forward((byte) distance, color));
         }  
+
+        [TestCase("e2", 7, 3, "h2")]
+        [TestCase("d6", 0, 3, "a6")]
+        [TestCase("e2", 6, 0, "e2")]
+        [TestCase("h6", 1, 7, "a6")]
+        public void GoInDirectionOf_CorrectSituation_ReturnsExpectedPosition(string positionString, int column, int distance, string expectedPositionString) {
+            var position = new Position(positionString);
+            var expectedPosition = new Position(expectedPositionString); 
+            Assert.AreEqual(expectedPositionString, position.GoInDirectionOf((byte) column, (byte) distance).ToString());
+        }
     }
 }
