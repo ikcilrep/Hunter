af409194579d674af379262cb93c9ae290841031
ikcilrep <frajerzycki@protonmail.com>
2020-02-27 21:32:02 +0100

    Add parsing Castling from string

--- Chess/Moves/IMove.cs
+++ Chess/Moves/IMove.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Chess.Pieces;
+using System.Linq;
 
 namespace Chess.Moves
 {
@@ -12,6 +13,21 @@ namespace Chess.Moves
         public static bool IsCapture(IMove move) => (move is Promotion && ((Promotion)move).IsCapture)
             || (move is Move && ((Move)move).IsCapture)
             || move is EnPassant;
+
+        public static IMove ParseMove(string moveString, Board board)
+        {
+            var isShortCastling = moveString.Equals("O-O");
+            var isLongCastling = moveString.Equals("O-O-O");
+            if (isShortCastling || isLongCastling)
+            {
+                var king = (King)board.Pieces.Values.Where(p => p.Color == !board.LastMove.Piece.Color && p is King).First();
+                if (Castling.IsCastling(king, isLongCastling, board))
+                {
+                    return new Castling(king, isLongCastling, board);
+                }
+            }
+            throw new NotImplementedException();
+        }
 
     }
 }
