f0a41bf0668b19db7431604841ae13c538cadfac
ikcilrep <frajerzycki@protonmail.com>
2020-02-27 22:22:04 +0100

    Add parsing Promotion from string

--- Chess/Moves/IMove.cs
+++ Chess/Moves/IMove.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Chess.Pieces;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Chess.Moves
 {
@@ -18,13 +19,47 @@ namespace Chess.Moves
         {
             var isShortCastling = moveString.Equals("O-O");
             var isLongCastling = moveString.Equals("O-O-O");
+            var color = !board.LastMove.Piece.Color;
             if (isShortCastling || isLongCastling)
             {
-                var king = (King)board.Pieces.Values.Where(p => p.Color == !board.LastMove.Piece.Color && p is King).First();
+                var king = (King)board.Pieces.Values.Where(p => p.Color == color && p is King).First();
                 if (Castling.IsCastling(king, isLongCastling, board))
                 {
                     return new Castling(king, isLongCastling, board);
                 }
+            }
+            var pawnPromotionRegex = new Regex(@"^([a-h]x)?[a-h][1-8]=[QBNRK]$");
+            if (pawnPromotionRegex.IsMatch(moveString))
+            {
+                var isCapture = moveString[1] == 'x';
+                var to = new Position(moveString.Substring(moveString.Length - 4, moveString.Length - 2));
+                Position from;
+                from = to.Behind(color);
+                if (isCapture)
+                {
+                    from = new Position(from.Row, Position.ParseColumn(moveString[0]));
+                }
+                var piece = board.Pieces[from];
+                Pawn pawn;
+                if (piece is Pawn)
+                {
+                    pawn = (Pawn)piece;
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+                var promotedPawn = Piece.ParsePiece(moveString[moveString.Length - 1], color);
+                var promotion = new Promotion(pawn, promotedPawn, to, board);
+                if (pawn.IsMovePossible(promotion, board))
+                {
+                    return promotion;
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+
             }
             throw new NotImplementedException();
         }
