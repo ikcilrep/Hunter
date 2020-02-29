96a6c2c945a0b127ec1587655528d8696e663665
ikcilrep <frajerzycki@protonmail.com>
2020-02-29 23:24:38 +0100

    Move chessmen moves parsing methods to another class

---
+++ Chess/Moves/MoveParser/ChessmenMoveParser.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using Chess.Pieces;
+
+namespace Chess.Moves
+{
+    public static class ChessmenMoveParser
+    {
+        internal static Castling ParseCastling(bool isLong, bool color, Board board)
+        {
+            var king = (King)board.Pieces.Values.Where(p => p.Color == color && p is King).First();
+            if (Castling.IsCastling(king, isLong, board))
+            {
+                return new Castling(king, isLong, board);
+            }
+            throw new ArgumentException();
+        }
+
+
+        private static IEnumerable<Piece> SelectPiecesByPosition(string moveString, IEnumerable<Piece> pieces, Board board)
+        {
+            Func<Piece, bool> selectPiece;
+            if (moveString[1] < 'a')
+            {
+                var row = Position.ParseRow(moveString[1]);
+                selectPiece = p => board.FindPiece(p).Row == row;
+            }
+            else
+            {
+                var column = Position.ParseColumn(moveString[1]);
+                selectPiece = p => board.FindPiece(p).Column == column;
+            }
+
+            return pieces.Where(selectPiece);
+        }
+
+        internal static IMove ParseChessmanMove(string moveString, bool color, Board board)
+        {
+            var to = MoveParser.GetToPosition(moveString);
+            var isPieceType = Piece.ParsePieceTypePredicate(moveString[0]);
+            var pieces = board.Pieces.Values.Where(p => isPieceType(p) && p.Color == color && p.PossibleMoves(board).Count(m => m.To == to) > 0);
+            if (moveString.Length > 3 && moveString[1] != 'x')
+            {
+                pieces = SelectPiecesByPosition(moveString, pieces, board);
+            }
+
+            if (pieces.Count() != 1)
+            {
+                throw new ArgumentException();
+            }
+
+            var piece = pieces.First();
+            var move = new Move(piece, to, board);
+            if (piece.IsMovePossible(move))
+            {
+                return move;
+            }
+            throw new ArgumentException();
+        }
+    }
+}
--- Chess/Moves/MoveParser/MoveParser.cs
+++ Chess/Moves/MoveParser/MoveParser.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Linq;
 using System.Text.RegularExpressions;
-using System.Collections.Generic;
-using Chess.Pieces;
 
 namespace Chess.Moves
 {
@@ -12,17 +9,7 @@ namespace Chess.Moves
         {
             return new Position(moveString.Substring(moveString.Length - 2));
         }
-       
 
-        private static Castling ParseCastling(bool isLong, bool color, Board board)
-        {
-            var king = (King)board.Pieces.Values.Where(p => p.Color == color && p is King).First();
-            if (Castling.IsCastling(king, isLong, board))
-            {
-                return new Castling(king, isLong, board);
-            }
-            throw new ArgumentException();
-        }
 
         public static IMove ParseMove(string moveString, Board board)
         {
@@ -31,7 +18,7 @@ namespace Chess.Moves
             var color = !board.LastMove.Piece.Color;
             if (isShortCastling || isLongCastling)
             {
-                return ParseCastling(isLongCastling, color, board);
+                return ChessmenMoveParser.ParseCastling(isLongCastling, color, board);
             }
 
             var pawnPromotionRegex = new Regex(@"^([a-h]x)?[a-h][1-8]=[QBNRK]$");
@@ -49,50 +36,11 @@ namespace Chess.Moves
             var chessmanMoveRegex = new Regex(@"^[QBRNK]([a-h]|[0-9])?x?[a-h][1-8]$");
             if (chessmanMoveRegex.IsMatch(moveString))
             {
-                return ParseChessmanMove(moveString, color, board);
+                return ChessmenMoveParser.ParseChessmanMove(moveString, color, board);
             }
             throw new FormatException();
         }
 
-        private static IEnumerable<Piece> SelectPiecesByPosition(string moveString, IEnumerable<Piece> pieces, Board board)
-        {
-            Func<Piece, bool> selectPiece;
-            if (moveString[1] < 'a')
-            {
-                var row = Position.ParseRow(moveString[1]);
-                selectPiece = p => board.FindPiece(p).Row == row;
-            }
-            else
-            {
-                var column = Position.ParseColumn(moveString[1]);
-                selectPiece = p => board.FindPiece(p).Column == column;
-            }
-
-            return pieces.Where(selectPiece);
-        }
-
-        private static IMove ParseChessmanMove(string moveString, bool color, Board board)
-        {
-            var to = GetToPosition(moveString);
-            var isPieceType = Piece.ParsePieceTypePredicate(moveString[0]);
-            var pieces = board.Pieces.Values.Where(p => isPieceType(p) && p.Color == color && p.PossibleMoves(board).Count(m => m.To == to) > 0);
-            if (moveString.Length > 3 && moveString[1] != 'x')
-            {
-                pieces = SelectPiecesByPosition(moveString, pieces, board);
-            }
-
-            if (pieces.Count() != 1)
-            {
-                throw new ArgumentException();
-            }
-
-            var piece = pieces.First();
-            var move = new Move(piece, to, board);
-            if (piece.IsMovePossible(move))
-            {
-                return move;
-            }
-            throw new ArgumentException();
-        }
+        
     }
 }
