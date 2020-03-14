e7d15746548e27f5d907a43ae2a671c2b1dc48e8
ikcilrep <frajerzycki@protonmail.com>
2020-03-14 23:44:49 +0100

    Remove debugging code

--- Chess/Program.cs
+++ Chess/Program.cs
@@ -9,24 +9,6 @@ namespace Chess
     {
         static void Main(string[] args)
         {
-            var board = new Board();
-            try
-            {
-                foreach (var piece in board.Pieces.Values)
-                {
-
-                    var possibleMoves = piece.PossibleMoves(board);
-
-                    Console.WriteLine($"Possible moves {possibleMoves.Count}");
-
-
-                }
-            }
-            catch (InvalidOperationException e)
-            {
-                Console.WriteLine(e);
-            }
-
             Console.WriteLine("Hello World!");
         }
     }
