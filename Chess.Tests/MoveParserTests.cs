fc2a06bb7aaeeb5957ad606171de01bd8a29caf0
ikcilrep <frajerzycki@protonmail.com>
2020-03-18 23:31:06 +0100

    Resign from testing for both colors

--- Chess.Tests/MoveParserTests.cs
+++ Chess.Tests/MoveParserTests.cs
@@ -8,47 +8,44 @@ namespace Chess.Tests
     [TestFixture]
     public class MoveParserTests
     {
-        private Board _whiteMoveBoard;
-        private Board _blackMoveBoard;
+        private static Board _board;
 
         [SetUp]
         public void Init()
         {
-            _whiteMoveBoard = new Board();
-            _whiteMoveBoard.MakeMove(new Move(_whiteMoveBoard["e2"], new Position("e4"), _whiteMoveBoard));
-            _whiteMoveBoard.MakeMove(new Move(_whiteMoveBoard["d7"], new Position("d5"), _whiteMoveBoard));
-            _whiteMoveBoard.MakeMove(new Move(_whiteMoveBoard["b1"], new Position("c3"), _whiteMoveBoard));
-            _whiteMoveBoard.MakeMove(new Move(_whiteMoveBoard["g8"], new Position("f6"), _whiteMoveBoard));
-            _whiteMoveBoard.MakeMove(new Move(_whiteMoveBoard["h2"], new Position("h4"), _whiteMoveBoard));
-            _whiteMoveBoard.MakeMove(new Move(_whiteMoveBoard["a7"], new Position("a6"), _whiteMoveBoard));
-            _whiteMoveBoard.MakeMove(new Move(_whiteMoveBoard["h4"], new Position("h5"), _whiteMoveBoard));
-            _whiteMoveBoard.MakeMove(new Move(_whiteMoveBoard["g7"], new Position("g5"), _whiteMoveBoard));
-
-            _blackMoveBoard = new Board();
-            _blackMoveBoard.MakeMove(new Move(_blackMoveBoard["e2"], new Position("e4"), _blackMoveBoard));
-            _blackMoveBoard.MakeMove(new Move(_blackMoveBoard["d7"], new Position("d5"), _blackMoveBoard));
-            _blackMoveBoard.MakeMove(new Move(_blackMoveBoard["b1"], new Position("c3"), _blackMoveBoard));
-            _blackMoveBoard.MakeMove(new Move(_blackMoveBoard["g8"], new Position("f6"), _blackMoveBoard));
-            _blackMoveBoard.MakeMove(new Move(_blackMoveBoard["f1"], new Position("d3"), _blackMoveBoard));
+            _board = new Board();
+            _board.MakeMove(new Move(_board["e2"], new Position("e4"), _board));
+            _board.MakeMove(new Move(_board["d7"], new Position("d5"), _board));
+            _board.MakeMove(new Move(_board["b1"], new Position("c3"), _board));
+            _board.MakeMove(new Move(_board["g8"], new Position("f6"), _board));
+            _board.MakeMove(new Move(_board["h2"], new Position("h4"), _board));
+            _board.MakeMove(new Move(_board["a7"], new Position("a6"), _board));
+            _board.MakeMove(new Move(_board["h4"], new Position("h5"), _board));
+            _board.MakeMove(new Move(_board["g7"], new Position("g5"), _board));
         }
 
-        private static void ParseMove_CorrectNotation_ReturnExpectedMove(string moveString,
-                                                                         string expectedFromPositionString,
-                                                                         string expectedToPositionString,
-                                                                         bool isEnPassant,
-                                                                         Board board)
+        [TestCase("Nxd5", "c3", "d5", false)]
+        [TestCase("exd5", "e4", "d5", false)]
+        [TestCase("e5", "e4", "e5", false)]
+        [TestCase("Bb5", "f1", "b5", false)]
+        [TestCase("hxg6", "h5", "g6", true)]
+
+        public static void ParseMove_CorrectNotation_ReturnExpectedMove(string moveString,
+                                                                                 string expectedFromPositionString,
+                                                                                 string expectedToPositionString,
+                                                                                 bool isEnPassant)
         {
 
             IMove expectedMove;
             if (isEnPassant)
             {
-                expectedMove = new EnPassant((Pawn)board[expectedFromPositionString], new Position(expectedToPositionString), board);
+                expectedMove = new EnPassant((Pawn)_board[expectedFromPositionString], new Position(expectedToPositionString), _board);
             }
             else
             {
-                expectedMove = new Move(board[expectedFromPositionString], new Position(expectedToPositionString), board);
+                expectedMove = new Move(_board[expectedFromPositionString], new Position(expectedToPositionString), _board);
             }
-            var move = MoveParser.ParseMove(moveString, board);
+            var move = MoveParser.ParseMove(moveString, _board);
 
             Assert.That(move.Equals(expectedMove));
         }
@@ -61,7 +58,7 @@ namespace Chess.Tests
         [TestCase("Bb53")]
         public void ParseMove_IncorrectNotation_ThrowFormatException(string moveString)
         {
-            Assert.Throws<FormatException>(() => MoveParser.ParseMove(moveString, _whiteMoveBoard));
+            Assert.Throws<FormatException>(() => MoveParser.ParseMove(moveString, _board));
         }
 
         [TestCase("Nd5")]
@@ -70,30 +67,7 @@ namespace Chess.Tests
         [TestCase("d5")]
         public void ParseMove_IncorrectMove_ThrowArgumentException(string moveString)
         {
-            Assert.Throws<ArgumentException>(() => MoveParser.ParseMove(moveString, _whiteMoveBoard));
-        }
-
-        [TestCase("Nxd5", "c3", "d5", false)]
-        [TestCase("exd5", "e4", "d5", false)]
-        [TestCase("e5", "e4", "e5", false)]
-        [TestCase("Bb5", "f1", "b5", false)]
-        [TestCase("hxg6", "h5", "g6", true)]
-
-        public void ParseMove_CorrectNotationWhiteMoves_ReturnExpectedMove(string moveString, string expectedFromPositionString, string expectedToPositionString, bool isEnPassant)
-        {
-            ParseMove_CorrectNotation_ReturnExpectedMove(moveString, expectedFromPositionString, expectedToPositionString, isEnPassant, _whiteMoveBoard);
-        }
-
-
-        [TestCase("Nxe4", "f6", "e4")]
-        [TestCase("dxe4", "d5", "e4")]
-        [TestCase("d4", "d5", "d4")]
-        [TestCase("Bg4", "c8", "g4")]
-
-
-        public void ParseMove_CorrectNotationBlackMoves_ReturnExpectedMove(string moveString, string expectedFromPositionString, string expectedToPositionString)
-        {
-            ParseMove_CorrectNotation_ReturnExpectedMove(moveString, expectedFromPositionString, expectedToPositionString, false, _blackMoveBoard);
+            Assert.Throws<ArgumentException>(() => MoveParser.ParseMove(moveString, _board));
         }
 
         [TestCase("Nxd5")]
@@ -105,7 +79,7 @@ namespace Chess.Tests
 
         public void ParseMove_CorrectNotation_ConvertsBackToSameStringRepresentation(string moveString)
         {
-            var move = MoveParser.ParseMove(moveString, _whiteMoveBoard);
+            var move = MoveParser.ParseMove(moveString, _board);
             Assert.AreEqual(moveString, move.ToString());
         }
     }
