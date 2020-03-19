732e2f776d42c840b7d5b609dec67ee9f1bffb95
ikcilrep <frajerzycki@protonmail.com>
2020-03-19 18:34:19 +0100

    Add Bishop.PossibleMoves test

---
+++ Chess.Tests/BishopTests.cs
@@ -0,0 +1,40 @@
+using System.Linq;
+using Chess.Moves;
+using Chess.Pieces;
+using NUnit.Framework;
+
+namespace Chess.Tests
+{
+    [TestFixture]
+    public class BishopTests
+    {
+        private static Board _board;
+
+        [SetUp]
+        public void Init()
+        {
+            _board = new Board();
+            _board.MakeMove(new Move(_board["e2"], new Position("e4"), _board));
+            _board.MakeMove(new Move(_board["e7"], new Position("e5"), _board));
+            _board.MakeMove(new Move(_board["d2"], new Position("d4"), _board));
+            _board.MakeMove(new Move(_board["d7"], new Position("d5"), _board));
+            _board.MakeMove(new Move(_board["c1"], new Position("g5"), _board));
+            _board.MakeMove(new Move(_board["c7"], new Position("c6"), _board));
+            _board.MakeMove(new Move(_board["f1"], new Position("c4"), _board));
+            _board.MakeMove(new Move(_board["a7"], new Position("a6"), _board));
+        }
+
+        [TestCase("c4", new string[]{"Bb5", "Bxa6", "Bxd5", "Bb3", "Bd3", "Be2", "Bf1"})]
+        [TestCase("g5", new string[]{"Bf6", "Be7", "Bxd8", "Bh4", "Bh6", "Bf4", "Be3", "Bd2", "Bc1"})]
+        public void PossibleMoves_ReturnsCorrectMoves(string bishopPositionString, string[] expectedPossibleMoves)
+        {
+            var bishop = (Bishop)_board[bishopPositionString];
+            var possibleMoves = bishop.PossibleMoves(_board).Select(m => m.ToString()).ToHashSet();
+            var expectedPossibleMovesSet = expectedPossibleMoves.ToHashSet();
+            Assert.That(possibleMoves.SetEquals(expectedPossibleMovesSet));
+        }
+
+
+
+    }
+}
