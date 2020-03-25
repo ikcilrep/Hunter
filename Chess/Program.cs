cd115d83b06bebee5b3d8567df998ae96820e131
ikcilrep <frajerzycki@protonmail.com>
2020-03-25 02:37:12 +0100

    Add simple game playing for debugging

--- Chess/Program.cs
+++ Chess/Program.cs
@@ -8,18 +8,55 @@ namespace Chess
 {
     class Program
     {
+        private static IMove ReadMove(Board board)
+        {
+            var moveString = Console.ReadLine();
+            try
+            {
+                return MoveParser.ParseMove(moveString, board);
+            }
+            catch (Exception ex)
+            {
+                if (ex is FormatException || ex is ArgumentException)
+                {
+                    return null;
+                }
+                throw;
+            }
+
+
+        }
         static void Main(string[] args)
         {
             Board board = new Board();
-            MoveTree moveTree = new MoveTree(10, board);
+            MoveTree moveTree = new MoveTree(2, board);
             var i = 1;
             while (true)
             {
-                Console.WriteLine($"{i}. {moveTree.BestMove}");
+                var bestMove = moveTree.BestMove;
+                Console.WriteLine($"{i}.\t{bestMove}");
+                board.MakeMove(bestMove);
 
-                var move = MoveParser.ParseMove(Console.ReadLine(), board);
-                board.MakeMove(move);
+
+                moveTree = moveTree[bestMove];
                 moveTree.Extend(board);
+
+                IMove move = null;
+                while (move == null)
+                {
+                    Console.Write("\t");
+                    move = ReadMove(board);
+                    if (move == null)
+                    {
+                        Console.WriteLine("Incorrect move, enter correct one.");
+                    }
+                }
+
+                board.MakeMove(move);
+
+                moveTree = moveTree[move];
+                moveTree.Extend(board);
+
                 i++;
             }
         }
